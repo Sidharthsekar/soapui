514830b697798ba5480fcb4137d3b30850e5dbcb
Sidharthsekar <sidharth.sekar@gmail.com>
2021-03-01 19:47:48 +0530

    initial commit

---
+++ src/main/java/SoapUiDevOps/DevOpsSoap/App.java
@@ -0,0 +1,15 @@
+package SoapUiDevOps.DevOpsSoap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String checkTwo() {
+    	return "Data transfered successfully - non static";
+    }
+    public static String checkOne() {
+    	return "Data transfered successfully - static";
+    }
+}
